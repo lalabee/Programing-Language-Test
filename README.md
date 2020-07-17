cb5f9d96a0a346e23b279bf9bb9e5684369bc5cb
lalabee <onlinelassie@gmail.com>
2020-07-16 19:13:19 -0700

    Add All Files

---
+++ README.md
@@ -0,0 +1,5 @@
+## Programming Language Test
+###Authored by Lauralee Green July 16, 2020 In response to the Second Code Review at Epicodus' Intro Into Programming Class for the May, 2020 Cohort.
+###I used HTML, CSS, JavaScript, jQuery, and Bootstrap to create what I have so far.
+###It was built with Open Source guidelines like those of the MIT Open Source Licence.
+###If you want to open this test, you are on your own. Git hub has me completely baffled! Two weeks ago I thought I was in control. Today, I am not.
---
+++ js/styles.js
@@ -0,0 +1,32 @@
+/*Questions for Programming Languages*/
+$(document).ready(function() {
+ /* $("button").click (function() {
+    $(".storyShowing").toggle();
+    $(".storyHidden").toggle();*/
+  $("startButton").click(display){
+  $("display")= ("containerShowing");
+  )}}
+  $("#formOne").submit(function(event) {
+    const question1Input = $("input#question1").val();
+    const question2Input = $("input#question2").val();
+    const painterInput = $("input#painter").val();
+    const exclamationInput = $("input#exclamation").val();
+    const transportationInput = $("input#transportation").val();
+    const applianceInput = $("input#appliance").val();
+  });
+
+    $(".question1").text(question1Input);
+    $(".question2").text(question2Input);
+    $(".painter").text(painterInput);
+    $(".exclamation").text(exclamationInput);
+    $(".transportation").text(transportationInput);
+    $(".appliance").text(applianceInput);
+
+
+   $(".clickable").click(function()) {
+      $(".storyShowing").toggle();
+      $(".storyHidden").toggle();
+  }
+  $("event").preventDefault();
+
+  }
