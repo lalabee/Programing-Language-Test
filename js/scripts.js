1a0283da932a4b0f0e2baaa6ed9c78b2405928a9
lalabee <onlinelassie@gmail.com>
2020-07-20 01:17:47 -0700

    ADD Changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,23 @@
-$(document).ready(function() { 
-  $("p").click(function() {
-    $("magic").toggle();
-    $("formOne").toggle();
-  })};
-("#formOne").submit(function (event) {
+$(document).ready(function() {
+$(".clickable").click(function() {
+  $("storyShowing").toggle();
+  $("storyHidden").toggle();
+});
+$("form#formOne").submit(function (event) {
   const question1Input = $("input#question1").val();
   const question2Input = $("input#question2").val();
   const painterInput = $("input#painter").val();
   const exclamationInput = $("input#exclamation").val();
   const transportationInput = $("input#transportation").val();
   const applianceInput = $("input#appliance").val();
-  
-    $(".question1").text(question1Input); 
-    $(".question2").text(question2Input);
-    $(".painter").text(painterInput);
-    $(".exclamation").text(exclamationInput),
-    $(".transportation").text(transportationInput),
-    $(".appliance").text(applianceInput);
-    
-    $("#story").show();
+
+    $(".question1").text("question1Input"); 
+    $(".question2").text("question2Input");
+    $(".painter").text("painterInput");
+    $(".exclamation").text("exclamationInput"),
+    $(".transportation").text("transportationInput"),
+    $(".appliance").text("applianceInput");
 });
-});
+    $("event").preventDefault (); {
+}
+})
