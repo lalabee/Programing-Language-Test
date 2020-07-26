61277bde2677dbd4bde1f9b59b1a10842bf72bc5
lalabee <onlinelassie@gmail.com>
2020-07-25 20:13:49 -0700

    Update js folder and files

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,19 @@
-$(document).ready(function() {
-$(".clickable").click(function() {
-  $("storyShowing").toggle();
-  $("storyHidden").toggle();
-});
-$("form#formOne").submit(function (event) {
+$(document).ready(function () {
+  $("form#formOne").submit(function (event) {
+
   const question1Input = $("input#question1").val();
   const question2Input = $("input#question2").val();
   const painterInput = $("input#painter").val();
-  const exclamationInput = $("input#exclamation").val();
   const transportationInput = $("input#transportation").val();
   const applianceInput = $("input#appliance").val();
 
-    $(".question1").text("question1Input"); 
-    $(".question2").text("question2Input");
-    $(".painter").text("painterInput");
-    $(".exclamation").text("exclamationInput"),
-    $(".transportation").text("transportationInput"),
-    $(".appliance").text("applianceInput");
-});
-    $("event").preventDefault (); {
-}
-})
+    $(".js-question1").text(question1Input); 
+    $(".js-question2").text(question2Input);
+    $(".js-painter").text(painterInput);
+    $(".js-transportation").text(transportationInput);
+    $(".js-appliance").text(applianceInput);
+  
+    $("#story").show();
+    event.preventDefault();
+  
+  })})
